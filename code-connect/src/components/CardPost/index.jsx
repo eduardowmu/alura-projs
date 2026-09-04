e3f5efc9e2498713b437b7abbb9d2b5305a9f658
Eduardo Watanabe <eduardo.watanabe@vilt.local>
2026-09-03 22:48:15 -0300

    nao funcionou usar o remotrePatterns

---
+++ code-connect/src/components/CardPost/index.jsx
@@ -0,0 +1,28 @@
+import { Avatar } from "../Avatar"
+import Image from "next/image"
+
+export const CardPost = ({post}) => { 
+    return (
+        <article>
+            <header>
+                <figure>
+                    <Image 
+                        src={post.cover} 
+                        width={438} 
+                        height={133} 
+                        alt={`Capa do post de titulo: ${post.title}`}
+                    />
+                </figure>
+            </header>
+            <section>
+                <h2>{post.title}</h2>
+                <p>{post.body}</p>
+            </section>
+            <footer>
+                <Avatar 
+                    imageSrc={post.author.avatar} 
+                    name={post.author.username}/>
+            </footer>
+        </article>
+    );
+};
